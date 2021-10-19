c17f7ae8ecfa25a07000a34facd992dd0004a9ab
Alex Gainza <52041439+alesgainza@users.noreply.github.com>
2021-10-19 22:22:09 +0200

    Add files via upload

---
+++ build/precache-manifest.e8f80e809a6e03d5202e0963409b35ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbe9130e946117a9ebd66c43e72f40e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45d2cfa2203920e2e40b",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dec9f6790e5f40270b0c",
+    "url": "/static/css/main.f4480a15.chunk.css"
+  },
+  {
+    "revision": "45d2cfa2203920e2e40b",
+    "url": "/static/js/2.19cbfd1e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.19cbfd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec9f6790e5f40270b0c",
+    "url": "/static/js/main.9829eeca.chunk.js"
+  },
+  {
+    "revision": "024c6d9d0a197a1cbf3b",
+    "url": "/static/js/runtime-main.970325b7.js"
+  }
+]);
